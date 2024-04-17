2c14e75a44a4f97af08d587fe098f785b367762e
Daniel David <148503968+ddavid37@users.noreply.github.com>
2024-04-17 12:58:25 -0400

    Discrete Math & Linear Algebra

---
+++ Discrete Math/Calculator of Basic Operations/Calculator/Calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+	double num1, num2;
+	char operation;
+
+	cout << "------------\nCalculator:\n------------\n\nEnter the first number, operation, and than the second number:\n";
+	cout << "At the end, press enter\n";
+
+	cin >> num1 >> operation >> num2;
+
+	switch (operation) {
+
+	case '+': cout << "\n" << "The resuilt is: " << num1 + num2 << endl; break;
+	case '-': cout << "\n" << "The resuilt is: " << num1 - num2 << endl; break;
+	case '*': cout << "\n" << "The resuilt is: " << num1 * num2 << endl; break;
+	case '/': cout << "\n" << "The resuilt is: " << num1 / num2 << endl; break;
+	case '%': cout << "\n" << "The resuilt is: " << static_cast<int>(num1) % static_cast<int>(num2); break;
+
+
+	default:cout << "Not valid operation!" << endl;
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
